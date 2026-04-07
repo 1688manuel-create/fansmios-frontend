878b70789771702640a7a653e265f4492acddec0
1688manuel-create <1688.manuel@gmail.com>
2026-04-07 13:12:49 -0700

    Fix: Ruta de globals.css y candados listos

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
 // 🔥 1. IMPORTACIONES DEL MOTOR DE IDIOMAS
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
